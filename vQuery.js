0a8ec0c915e7b16d0fa8ab7c368ade4f895f705c
luoxiaowang <15927510358@163.com>
2015-03-17 15:24:52 +0800

    test

--- vQuery.js
+++ vQuery.js
@@ -1,3 +1,19 @@
 /**
  * Created by lenovo on 2015/3/17.
  */
+
+function myAddEvent(obj,sEv,fn){
+    if(obj.attachEvent){
+        obj.attachEvent('on'+sEv,fn);
+    }else{
+        obj.addEventListener(sEv,fn,false);
+    }
+}
+
+function VQuery(vArg){
+    switch (typeof vArg){
+        case 'function' :
+            myAddEvent(window,'load',vArg);
+            break;
+    }
+}
