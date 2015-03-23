348278c2c1ab6517d488b009013295e052738ed4
luoxiaowang <15927510358@163.com>
2015-03-23 15:06:22 +0800

    新增事件冒泡阻止、bind函数，animate、extend等

--- VQuery.js
+++ VQuery.js
@@ -1,9 +1,6 @@
 /**
  * Created by lenovo on 2015/3/17.
  */
-/**
- * Created by lenovo on 2015/3/17.
- */
 
 /**
  * 绑定事件
@@ -14,9 +11,21 @@
 function myAddEvent(obj,sEv,fn){
     //IE下会导致this被改成window,使用call
     if(obj.attachEvent){
-        obj.attachEvent('on'+sEv,fn);
+        obj.attachEvent('on'+sEv,function(){
+            //阻止默认事件,阻止冒泡
+            if(false == fn.call(obj)){
+                event.cancelBubble = true;
+                return false;
+            }
+        });
     }else{
-        obj.addEventListener(sEv,fn,false);
+        obj.addEventListener(sEv,function(ev){
+            //阻止默认事件，阻止冒泡
+            if(false == fn.call(obj)){
+                ev.cancelBubble = true;
+               ev.preventDefault();
+            }
+        },false);
     }
 }
 
@@ -115,8 +124,108 @@ VQuery.prototype.css = function(attr , value){
             this.elements[i].style[attr] = value;
         }
     }else{
+        if(typeof attr == 'string'){
             return getStyle(this.elements[0],attr);
+        }else{
+            //如果是json对象值
+            for(i = 0 ;i<this.elements.length;i++){
+                var k='';
+                for(k in attr){
+                    this.elements[i].style[k] = attr[k];
+                }
+            }
+        }
     }
+    //为了链式操作
+    return this;
+}
+
+
+VQuery.prototype.toggle = function(){
+    var i  = 0;
+    var _arguments = arguments;
+    for(i = 0 ; i < this.elements.length ; i++){
+        addToggle(this.elements[i]);
+    }
+
+    function addToggle(obj){
+        var count = 0 ;
+        myAddEvent(obj,'click',function(){
+            _arguments[count++%_arguments.length].call(obj);
+        });
+    }
+}
+
+VQuery.prototype.attr = function(attr,value){
+    if(arguments.length == 2){
+        var i = 0;
+        for(i = 0; i<this.elements.length;i++){
+            this.elements[i][attr] = value;
+        }
+    }else{
+        return this.elements[0][attr];
+    }
+}
+
+
+VQuery.prototype.eq = function(n){
+    return $(this.elements[n]);
+}
+
+
+function appendArr(arr1,arr2){
+    var i = 0;
+    for(i = 0 ; i<arr2.length ; i++){
+        arr1.push(arr2[i]);
+    }
+}
+
+VQuery.prototype.find = function(str){
+    var i = 0;
+    var aResult = [];
+    for(i = 0 ; i < this.elements.length ; i++ ){
+        switch (str.charAt(0)){
+            case "." :
+                var aEle = getByClass(this.elements[i],str.substring(1));
+                aResult = aResult.concat(aEle);
+                break;
+            default :
+                var aEle = this.elements[i].getElementsByTagName(str);  //返回的并非一个数组，而是一个HtmlCollection
+                /*aResult = aResult.concat(aEle);*/
+                appendArr(aResult , aEle);
+
+        }
+    }
+
+    var newVquery = $();
+    newVquery.elements=aResult;
+    return newVquery;
+ }
+
+function getIndex(obj){
+    var aBrother = obj.parentNode.children;
+    var i = 0;
+    for(i = 0 ; i<aBrother.length ; i++){
+        if(aBrother[i]==obj){
+            return i;
+        }
+    }
+}
+
+VQuery.prototype.index = function(){
+    return getIndex(this.elements[0]);
+}
+
+VQuery.prototype.bind = function (sEv,fn) {
+    var i = 0;
+    for(i = 0 ; i<this.elements.length ; i++){
+        myAddEvent(this.elements[i],sEv,fn);
+    }
+}
+
+
+VQuery.prototype.extend = function (name,fn) {
+    VQuery.prototype[name] = fn;
 }
 
 function $(vArg){
